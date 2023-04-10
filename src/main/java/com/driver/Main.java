db5498eba9ac3a1e9d57a5a525938b19a50f0855
ShanawazSDE <103483900+ShanawazSDE@users.noreply.github.com>
2023-04-10 17:43:20 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
-public class Main {
-  
+public class Main
+{
+  public static void main (String[]args)
+  {
+    RWOnly obj = new RWOnly();
+    //obj.var = 1; error: var has private access in RWOnly
+    obj.setVar(1);
+    System.out.println(obj.getVar());
+  }
 }
